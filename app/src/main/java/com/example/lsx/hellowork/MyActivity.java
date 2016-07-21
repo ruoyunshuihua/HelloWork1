5ecbedd70a933bee541cc1194b3bf4a75f7d157c
yfx <25832709782@qq.com>
2016-07-21 16:48:02 +0800

    Commit

--- app/src/main/java/com/example/lsx/hellowork/MyActivity.java
+++ app/src/main/java/com/example/lsx/hellowork/MyActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -26,6 +28,15 @@ public class MyActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Button button = (Button) findViewById(R.id.content_my_button1);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("lsx", "xxxxxxxxxxxxxxxxxxxx");
+                }
+            });
+        }
     }
 
     @Override
